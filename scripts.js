d05ad2b4b4ec59058ef645f06ef702096152a04c
Brandon Little <brandonlittle1999@hotmail.com>
2020-05-02 19:21:39 +0100

    Added the ability to count down. Need to correct moving from work to break timer.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,86 @@
 let timer = document.getElementById("timer");
-let startBtn = document.getElementById("startBtn");
-let reloardBtn = document.getElementById("startBtn");
 
 let counter = 0;
 let workTime = 25*60;
+let breakTime = 5*60;
+let indicator;
+
+timer.innerHTML = "00:00";
+
+
+let beginTimer = setInterval(workTimer, 100);
+let beginBreak;
+
+
+function stopWork(){
+    clearInterval(beginTimer);
+}
+
+function stopBreak(){
+    clearInterval(beginBreak);
+}
+
 
 function convertSeconds(s){
     let minutes = Math.floor(s / 60);
     let seconds = s % 60;
 
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
     if (seconds < 10){
         seconds = "0" + seconds;
     }
-    return `${minutes}:${seconds}`;
+
+    let time = `${minutes}:${seconds}`;
+
+    // if (time === "00:00"){
+    //     switch(indicator){
+    //         case 1:
+    //             breakTimer();
+    //             break;
+    //         case 2:
+    //             workTimer();
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // } else {
+    return time;
+    // }
 }
 
-timer.innerHTML = convertSeconds((workTime -counter));
-
-function secondCount() {
+function breakTimer() {
+    indicator = 2;
     counter++
+    timer.innerHTML = convertSeconds((breakTime - counter));
+    if (timer.innerHTML === "00:00"){
+        stopBreak();
+        // workTimer();
+        let beginTimer = setInterval(workTimer, 1);
+    }
+}
+
+function workTimer() {
+    indicator = 1;
+    counter++
+    console.log(timer.innerHTML);
     timer.innerHTML = convertSeconds((workTime - counter));
-
+    if (timer.innerHTML === "00:00"){
+        stopWork();
+        // breakTimer();
+        let beginBreak = setInterval(breakTimer, 1);
+    }
 }
 
-function starTimer() {
-    let secondsTimer = setInterval(secondCount, 1000);
-    
-}
+// function startTimer() {
+//     // setInterval(setInterval(workTimer, 1), (workTime * 1000));
+// }
 
-starTimer();
+// function startBreakTimer() {
+// }
 
+
+// startTimer();
+
+workTimer()
