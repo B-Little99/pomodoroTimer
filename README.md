8ed51c633b6238fc9527a2bb93c750cfe107d3fc
Brandon Little <brandonlittle1999@hotmail.com>
2020-05-03 19:55:46 +0100

    Resolved the error which caused the timer to go into the negatives. Now once reaching 0 it will move on to the break/work timer.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This application is intended to be used as a productivity tool by the user where they work for 25 minutes and then have a 5 minute break.
 
+Issues:
+
+Clicking on the start then clicking on it again means that the user can speed up the timer and make the timer go quicker. This is not helpful if the user were to exploit such a measure to be counterproductive.
+
+
--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 let timer = document.getElementById("timer");
-
+let start = document.getElementById("startBtn");
+let stop = document.getElementById("stopBtn");
 let counter = 0;
 let workTime = 25*60;
 let breakTime = 5*60;
-let indicator;
+let beginTimer;
+let beginBreak;
 
 timer.innerHTML = "00:00";
 
+// These functions start the work/break sessions for the user.
+function startWorkTimer(){
+    beginTimer = setInterval(workTimer, 1000);
+}
 
-let beginTimer = setInterval(workTimer, 100);
-let beginBreak;
+function startBreakTimer(){
+    beginBreak = setInterval(breakTimer, 1000)
+}
 
-
+// These two functions are used to stop the timer. It gives the impression of pausing it though, because the counter stays the same, which means when it starts again it can resume at the time it left off.
 function stopWork(){
-    clearInterval(beginTimer);
+    beginTimer = clearInterval(beginTimer);
 }
 
 function stopBreak(){
-    clearInterval(beginBreak);
+    beginbreak = clearInterval(beginBreak);
 }
 
-
+// The convertSeconds function takes in the total seconds of the current time and converts it to a format that is displayed using a template literal.
 function convertSeconds(s){
     let minutes = Math.floor(s / 60);
     let seconds = s % 60;
@@ -31,56 +38,33 @@ function convertSeconds(s){
     if (seconds < 10){
         seconds = "0" + seconds;
     }
-
     let time = `${minutes}:${seconds}`;
-
-    // if (time === "00:00"){
-    //     switch(indicator){
-    //         case 1:
-    //             breakTimer();
-    //             break;
-    //         case 2:
-    //             workTimer();
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // } else {
     return time;
-    // }
 }
 
+// The breakTimer function is used to display the break time and count down, where they will clear the interval, reset the counter and move on to the workTimer function once reaching 0.
 function breakTimer() {
-    indicator = 2;
     counter++
     timer.innerHTML = convertSeconds((breakTime - counter));
     if (timer.innerHTML === "00:00"){
+        counter = 0;
         stopBreak();
-        // workTimer();
-        let beginTimer = setInterval(workTimer, 1);
+        startWorkTimer();
     }
 }
 
+// The workTimer function is used to display the work time and count down, where they will clear the interval, reset the counter and move on to the breakTimer function once reaching 0.
 function workTimer() {
-    indicator = 1;
     counter++
-    console.log(timer.innerHTML);
     timer.innerHTML = convertSeconds((workTime - counter));
     if (timer.innerHTML === "00:00"){
+        counter = 0;
         stopWork();
-        // breakTimer();
-        let beginBreak = setInterval(breakTimer, 1);
+        startBreakTimer();
     }
 }
 
-// function startTimer() {
-//     // setInterval(setInterval(workTimer, 1), (workTime * 1000));
-// }
+start.addEventListener("click", startWorkTimer);
 
-// function startBreakTimer() {
-// }
-
-
-// startTimer();
-
-workTimer()
+stop.addEventListener("click", stopWork);
+stop.addEventListener("click", stopBreak)
